ca6efa9f075af49c631d67ecd082cbe6c83f7dc6
PrideZH <332842890@qq.com>
2022-01-26 21:43:35 +0800

    时间显示

--- components.d.ts
+++ components.d.ts
@@ -13,6 +13,7 @@ declare module 'vue' {
     ElDrawer: typeof import('element-plus/es')['ElDrawer']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElMenu: typeof import('element-plus/es')['ElMenu']
--- src/views/todo/index.vue
+++ src/views/todo/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Edit, Delete } from '@element-plus/icons-vue'
+import { Calendar, Delete, Edit } from '@element-plus/icons-vue'
 import { useTodoStore } from '@/store';
 import { ref } from 'vue';
 import Drawer from './components/Drawer.vue';
@@ -7,17 +7,32 @@ import Drawer from './components/Drawer.vue';
 const todoStore = useTodoStore();
 
 const drawer = ref<any>();
+
+const dateFormat = (date: Date): string => {
+  date = new Date(date);
+  const days: string[] = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
+  let res: string = '';
+  if (date.getFullYear() !== new Date().getFullYear()) {
+    res += date.getFullYear() + '年';
+  }
+  res += date.getMonth() + 1 + '月';
+  res += date.getDate() + '日';
+  res += days[date.getDay()];
+  res += ' ' + (date.getHours() >= 12 ? '下午' : '上午') + date.getHours() % 12;
+  res += ':' + date.getMinutes();
+  return res;
+};
 </script>
 
 <template>
-  <h1>今天计划</h1>
+  <h1>计划</h1>
   <el-button @click="drawer.open()">添加计划</el-button>
   <el-card v-for="todo in todoStore.todos" :key="todo.id" size="large">
     <div class="todo-item">
       <el-checkbox v-model="todo.completion"/>
       <div class="todo-body">
         <div class="todo-content">{{ todo.content }}</div>
-        {{ todo.deadline }}
+        <div class="todo-time" v-if="todo.deadline"><el-icon><calendar /></el-icon>{{ dateFormat(todo.deadline) }}</div>
       </div>
       <el-button type="primary" :icon="Edit" size="small" circle @click="drawer.open(todo)" />
       <el-button type="danger" :icon="Delete" size="small" circle @click="todoStore.del(todo.id)" />
@@ -36,5 +51,12 @@ const drawer = ref<any>();
 .todo-body {
   flex: 1;
   padding: 0 16px;
+}
+
+.todo-time {
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  color: #5e5e5e;
 }
 </style>
